7f20c75d0ff315e261ffcd3c43644c91c50a615b
alkuinvito <alkuinvito@tutanota.com>
2024-03-13 18:33:19 +0700

    fix: create error throw when not found

--- src/users/queries/getPublicProfile.ts
+++ src/users/queries/getPublicProfile.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/core/adapters/prisma";
 
 export default async function getPublicProfile(id: string) {
-  return prisma.user.findUnique({
+  return prisma.user.findUniqueOrThrow({
     where: {
       id: id,
     },
