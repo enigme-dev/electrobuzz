a10c5c016d3be9abbd52cb14dc8185a6b4bd60d8
alkuinvito <alkuinvito@tutanota.com>
2024-03-27 12:51:12 +0700

    fix: make image on update optional

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -70,7 +70,7 @@ export async function PATCH(req: NextRequest, { params }: IdParams) {
 
   if (userId.data !== params.id) {
     return buildErr(
-      "ErrUnauthorized",
+      "ErrForbidden",
       403,
       "not allowed to update other user's profile"
     );
@@ -81,25 +81,17 @@ export async function PATCH(req: NextRequest, { params }: IdParams) {
     return buildErr("ErrValidation", 400, input.error);
   }
 
-  let imageUrl = input.data.image;
+  let imageUrl;
   try {
-    const user = await getPrivateProfile(userId.data);
-
-    if (!input.data.image.startsWith(process.env.ASSETS_URL as string)) {
+    if (input.data.image && input.data.image.startsWith("data:image")) {
+      const user = await getPrivateProfile(userId.data);
       if (user?.image?.startsWith(process.env.ASSETS_URL as string)) {
         await deleteImg(user?.image);
       }
 
       imageUrl = await uploadImg(input.data.image);
     }
-
-    input.data.phoneVerified = user?.phoneVerified;
-    if (input.data.phone !== user?.phone) {
-      input.data.phoneVerified = false;
-    }
   } catch (e) {
-    deleteImg(removeImagePrefix(imageUrl));
-
     return buildErr("ErrUnknown", 500);
   }
 
@@ -108,7 +100,9 @@ export async function PATCH(req: NextRequest, { params }: IdParams) {
   try {
     await updateProfile(input.data, params.id);
   } catch (e) {
-    deleteImg(removeImagePrefix(imageUrl));
+    if (imageUrl) {
+      deleteImg(removeImagePrefix(imageUrl));
+    }
 
     if (e instanceof Prisma.PrismaClientKnownRequestError) {
       if (e.code === "P2002") {
