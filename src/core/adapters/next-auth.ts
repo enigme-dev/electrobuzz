c4dfea59f1e4e463a05c3f89b4026393b4409268
alkuinvito <alkuinvito@tutanota.com>
2024-04-04 11:22:22 +0700

    feat: add merchant id & admin role to token

--- src/core/adapters/next-auth.ts
+++ src/core/adapters/next-auth.ts
@@ -10,9 +10,12 @@ import { prisma } from "@/core/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 import getPrivateProfile from "@/users/queries/getPrivateProfile";
 import getAddressCount from "@/users/queries/getAddressCount";
+import getMerchantByUserId from "@/merchants/queries/getMerchantByUserId";
 
 interface IUser extends DefaultUser {
   isNewUser?: boolean;
+  isAdmin?: boolean;
+  merchantId?: string;
 }
 declare module "next-auth" {
   interface User extends IUser {}
@@ -33,15 +36,25 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async jwt({ token }) {
-      const profile = await getPrivateProfile(token.sub as string);
-      const addressCt = await getAddressCount(token.sub as string);
+    async jwt({ token, user }) {
+      if (user) {
+        const profile = await getPrivateProfile(user.id);
+        const addressCt = await getAddressCount(user.id);
 
-      const isRegistered =
-        Boolean(profile?.phone) && addressCt._count.addresses >= 1;
+        const isRegistered =
+          Boolean(profile?.phone) && addressCt._count.addresses >= 1;
+        if (!isRegistered) {
+          token.isNewUser = true;
+        }
 
-      if (!isRegistered) {
-        token.isNewUser = true;
+        token.isAdmin = profile?.isAdmin;
+
+        try {
+          const merchant = await getMerchantByUserId(user.id);
+          if (merchant) {
+            token.merchantId = merchant.merchantId;
+          }
+        } catch (e) {}
       }
 
       return token;
@@ -49,7 +62,10 @@ export const authOptions: NextAuthOptions = {
     async session({ session, token }) {
       if (session.user && token) {
         session.user.isNewUser = token.isNewUser;
+        session.user.merchantId = token.merchantId;
+        session.user.isAdmin = token.isAdmin;
       }
+
       return session;
     },
     async redirect({ baseUrl }) {
