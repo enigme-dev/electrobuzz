bf6c24694c856d5d4de98521a63eef47038c9c5a
alkuinvito <alkuinvito@tutanota.com>
2024-05-09 18:14:13 +0700

    feat: add get user bookings

--- src/core/lib/utils.ts
+++ src/core/lib/utils.ts
@@ -1,16 +1,31 @@
 import { z } from "zod";
 
 export function parseParams(searchParams: URLSearchParams) {
+  let startDate, endDate;
   const query = searchParams.get("query") ?? "";
-  const page = parseInt(searchParams.get("page") ?? "1");
+  const page = parseInt(searchParams.get("page") || "1");
   const skip = (page - 1) * 10;
+  const startDateParam = z.coerce
+    .date()
+    .safeParse(searchParams.get("start-date"));
+  if (startDateParam.success) {
+    startDateParam.data.setHours(0o0, 0o0, 0o0);
+    startDate = startDateParam.data;
+  }
+  const endDateParam = z.coerce.date().safeParse(searchParams.get("end-date"));
+  if (endDateParam.success) {
+    endDateParam.data.setHours(23, 59, 59);
+    endDate = endDateParam.data;
+  }
 
-  return { query, page, skip };
+  return { query, page, skip, startDate, endDate };
 }
 
 export const SearchParams = z.object({
   query: z.string().default("").optional(),
   page: z.number().default(1).optional(),
+  startDate: z.coerce.date().optional(),
+  endDate: z.coerce.date().optional(),
 });
 
 export type SearchParams = z.infer<typeof SearchParams>;
