7ea637331abb39adae81659a30da4da43e86160d
alkuinvito <alkuinvito@tutanota.com>
2024-05-11 22:20:48 +0700

    feat: check if user is merchant

--- src/core/adapters/next-auth.ts
+++ src/core/adapters/next-auth.ts
@@ -1,28 +1,32 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { type GetServerSidePropsContext } from "next";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import {type GetServerSidePropsContext} from "next";
 import {
   getServerSession,
   type NextAuthOptions,
   type DefaultUser,
 } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { prisma } from "@/core/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
-import { getPrivateProfile } from "@/users/services/UserService";
+import {prisma} from "@/core/adapters/prisma";
+import {PrismaClient} from "@prisma/client";
+import {getPrivateProfile} from "@/users/services/UserService";
+import {getMerchant} from "@/merchants/services/MerchantService";
 
 interface IUser extends DefaultUser {
   isAdmin?: boolean;
+  isMerchant?: boolean;
 }
 
 declare module "next-auth" {
-  interface User extends IUser {}
+  interface User extends IUser {
+  }
 
   interface Session {
     user?: User;
   }
 }
 declare module "next-auth/jwt" {
-  interface JWT extends IUser {}
+  interface JWT extends IUser {
+  }
 }
 
 export const authOptions: NextAuthOptions = {
@@ -34,28 +38,35 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async jwt({ token, user, trigger, session }) {
+    async jwt({token, user, trigger, session}) {
       if (user) {
+        // check if user is admin
         const profile = await getPrivateProfile(user.id);
         token.isAdmin = profile?.isAdmin;
+
+        // check if user is registered as merchant
+        const merchant = await getMerchant(user.id);
+        token.isMerchant = Boolean(merchant?.merchantId)
       }
 
       if (trigger === "update") {
         if (session?.name) token.name = session.name;
         if (session?.image) token.image = session.image;
+        if (session?.isMerchant) token.isMerchant = session.isMerchant;
       }
 
       return token;
     },
-    async session({ session, token }) {
+    async session({session, token}) {
       if (session.user && token) {
-        session.user.isAdmin = token.isAdmin;
         session.user.id = token.sub ?? "";
+        session.user.isAdmin = token.isAdmin;
+        session.user.isMerchant = token.isMerchant;
       }
 
       return session;
     },
-    async redirect({ baseUrl }) {
+    async redirect({baseUrl}) {
       return baseUrl + "/register";
     },
   },
