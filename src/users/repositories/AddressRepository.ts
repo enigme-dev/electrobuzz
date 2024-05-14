ee4bcff6a59a2698f72a6ca8593bd3985a40401f
alkuinvito <alkuinvito@tutanota.com>
2024-05-14 20:28:46 +0700

    fix: check address total on every tran

--- src/users/repositories/AddressRepository.ts
+++ src/users/repositories/AddressRepository.ts
@@ -1,47 +1,58 @@
-import {BaseRepository} from "@/core/repositories/BaseRepository";
+import { BaseRepository } from "@/core/repositories/BaseRepository";
 
-import {AddressModel} from "@/users/types";
+import { AddressModel } from "@/users/types";
+import { Prisma } from "@prisma/client";
 
 export class AddressRepository extends BaseRepository {
-  static count(userId: string) {
-    return this.db.address.count({where: {userId: userId}});
-  }
-
   static create(userId: string, data: AddressModel) {
-    return this.db.address.create({
-      data: {
-        addressDetail: data.addressDetail,
-        addressCity: data.addressCity,
-        addressProvince: data.addressProvince,
-        addressZipCode: data.addressZipCode,
-        user: {
-          connect: {
-            id: userId
-          }
-        }
-      }
-    })
+    return this.db.$transaction(
+      async (tx) => {
+        const totalAddr = await tx.address.count({ where: { userId } });
+
+        if (totalAddr >= 3)
+          throw new Error("maximum number of addresses has reached");
+
+        const created = await tx.address.create({
+          data: {
+            addressDetail: data.addressDetail,
+            addressCity: data.addressCity,
+            addressProvince: data.addressProvince,
+            addressZipCode: data.addressZipCode,
+            user: {
+              connect: {
+                id: userId,
+              },
+            },
+          },
+        });
+        return created;
+      },
+      { isolationLevel: Prisma.TransactionIsolationLevel.Serializable }
+    );
   }
 
   static delete(id: string) {
-    return this.db.address.delete({where: {addressId: id}})
+    return this.db.address.delete({ where: { addressId: id } });
   }
 
   static findAll(userId: string) {
-    return this.db.address.findMany({
-      where: {
-        userId: userId,
-      },
-      select: {
-        addressId: true,
-        addressDetail: true,
-        addressZipCode: true,
-        addressCity: true,
-        addressProvince: true,
-        userId: false,
-        user: false,
-      },
-    });
+    return this.db.$transaction([
+      this.db.address.findMany({
+        where: {
+          userId: userId,
+        },
+        select: {
+          addressId: true,
+          addressDetail: true,
+          addressZipCode: true,
+          addressCity: true,
+          addressProvince: true,
+          userId: false,
+          user: false,
+        },
+      }),
+      this.db.address.count({ where: { userId } }),
+    ]);
   }
 
   static findOne(id: string) {
@@ -62,6 +73,6 @@ export class AddressRepository extends BaseRepository {
   }
 
   static update(id: string, data: AddressModel) {
-    return this.db.address.update({where: {addressId: id}, data})
+    return this.db.address.update({ where: { addressId: id }, data });
   }
 }
