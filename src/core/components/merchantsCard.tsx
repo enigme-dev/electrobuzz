520dddb1e8f71f170a039a0656387d92aca85b5c
Antonius Wisnu <fransiskus.wisnu@colearn.id>
2024-05-15 22:50:03 +0700

    refactor styles on merchantCard

--- src/core/components/merchantsCard.tsx
+++ src/core/components/merchantsCard.tsx
@@ -37,21 +37,33 @@ const MerchantsCard: React.FC<Props> = ({
         }}
         className="flex w-full shadow-sm sm:shadow-lg cursor-pointer  transition duration-500"
       >
-        <div className="flex gap-5 sm:gap-10 items-center p-3">
+        <div className="flex gap-5 sm:gap-10 items-center p-2 sm:p-3">
           <div>
             <Image
-              className="aspect-square p-3 rounded-full flex justify-center items-center"
+              className="aspect-square  rounded-full flex justify-center items-center"
               src={imgSource}
               alt={imgAlt}
-              width={85}
-              height={85}
+              width={70}
+              height={70}
             />
           </div>
-          <div className="grid place-items-start gap-1">
-            <div className="flex items-start sm:items-center gap-2 flex-col sm:flex-row">
-              <h1 className="text-sm sm:text-md font-semibold ">{merchName}</h1>
-              <p className="text-xs text-gray-400">{meterToKilometer} Km</p>
+          <div className="grid place-items-start">
+            <div className="flex items-center gap-4 w-[100%]">
+              <h1 className="text-xs sm:text-sm font-semibold max-w-[150px] overflow-hidden whitespace-nowrap">
+                {merchName}
+              </h1>
+              <p className="text-[0.6rem] sm:text-xs text-gray-400">
+                {meterToKilometer} Km
+              </p>
             </div>
+
+            <h2 className="text-gray-400 text-[0.6rem] sm:text-sm  flex items-center gap-2">
+              <MapPinIcon className="w-4" />
+              <p className="max-w-[200px] overflow-hidden whitespace-nowrap">
+                {location}
+              </p>
+            </h2>
+
             <div className="flex gap-4 items-center ">
               <h2 className="rounded-lg text-gray-400 text-[0.6rem]  sm:text-sm flex items-center gap-2">
                 <Lightbulb className="w-4" />
@@ -63,10 +75,6 @@ const MerchantsCard: React.FC<Props> = ({
                 </div>
               </h2>
             </div>
-            <h2 className="text-gray-400 text-[0.6rem] sm:text-sm  flex items-center gap-2 ">
-              <MapPinIcon className="w-4 " />
-              {location}
-            </h2>
           </div>
           {/* <div className="flex items-center gap-2">
             <p className="text-xs sm:text-sm">4,8/5</p>
