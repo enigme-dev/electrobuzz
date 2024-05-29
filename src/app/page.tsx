bc6dec2973d586fd99fef8859dcd037fa033be54
alkuinvito <alkuinvito@tutanota.com>
2024-05-29 18:40:07 +0700

    build: add merchants cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Banner } from "@/dashboard/banner";
 import Category from "@/category/container/category";
 import StepByStep from "@/step-by-step/container/stepBystep";
 import NearestMerchants from "@/NearestMerchants/container/NearestMerchants";
-import { useGeoLocation } from "@/core/hooks/useGeolocation";
 
 export default function Home() {
   return (
