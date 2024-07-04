0825b191e71f0386b4b67817191615727d9d27cb
alkuinvito <alkuinvito@tutanota.com>
2024-07-04 15:02:59 +0700

    build: add redis credentials

--- src/core/adapters/redis.ts
+++ src/core/adapters/redis.ts
@@ -2,4 +2,4 @@ import Redis from "ioredis";
 
 export const RedisClient = new Redis(process.env.REDIS_URL as string);
 
-export const CacheClient = new Redis("127.0.0.1");
+export const CacheClient = new Redis(process.env.REDIS_CACHE_URL as string);
